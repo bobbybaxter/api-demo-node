73b06db88f653463f00d7a69cf5d820db52ee63f
Bob Baxter <bob.e.baxter@gmail.com>
2025-08-02 10:40:55 -0500

    added delete and patch operations

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { createNewUser } from '../controllers/user/create-new-user';
+import { deleteUser } from '../controllers/user/delete-user';
 import { getUserById } from '../controllers/user/get-user-by-id';
+import { updateUser } from '../controllers/user/update-user';
 import { validateRequest } from '../middleware/validation-middleware';
-import { userCreateSchema, userIdSchema } from '../validation/schemas/user.schema';
+import { userCreateSchema, userIdSchema, userUpdateSchema } from '../validation/schemas/user.schema';
 
 const router = express.Router();
+
+router.delete('/:id', validateRequest(userIdSchema, 'params'), (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const user = deleteUser(id);
+
+  res.send(user);
+});
 
 router.get('/:id', validateRequest(userIdSchema, 'params'), (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
@@ -12,6 +21,18 @@ router.get('/:id', validateRequest(userIdSchema, 'params'), (req: Request, res: 
 
   res.send(user);
 });
+
+router.patch(
+  '/:id',
+  validateRequest(userIdSchema, 'params'),
+  validateRequest(userUpdateSchema, 'body'),
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const user = updateUser(id, req.body);
+
+    res.send(user);
+  },
+);
 
 router.post('/', validateRequest(userCreateSchema, 'body'), (req: Request, res: Response, next: NextFunction) => {
   const user = createNewUser(req.body);
--- src/validation/schemas/user.schema.ts
+++ src/validation/schemas/user.schema.ts
@@ -1,19 +1,17 @@
 import Joi from 'joi';
-import { dateSchema, emailSchema, idSchema, phoneSchema } from './shared.schema';
+import { emailSchema, idSchema, phoneSchema } from './shared.schema';
 
 const userBaseSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  firstName: Joi.string(),
+  lastName: Joi.string(),
   email: emailSchema,
   phone: phoneSchema,
 });
 
-export const userCreateSchema = userBaseSchema;
+export const userCreateSchema = userBaseSchema.fork(['firstName', 'lastName'], (schema) => schema.required());
 
 export const userIdSchema = Joi.object({
   id: idSchema,
 });
 
-export const userUpdateSchema = userBaseSchema.append({
-  id: idSchema,
-});
+export const userUpdateSchema = userBaseSchema;
